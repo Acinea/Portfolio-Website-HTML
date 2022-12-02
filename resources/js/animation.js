391d04ba8d72a162125bf28b7d3fb25d1b13a043
James Kennedy <acinea.kennedy@gmail.com>
2022-12-02 09:48:56 -0500

    Added Logos to About Page

---
+++ resources/js/animation.js
@@ -0,0 +1,18 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".work1-container, .work2-container, .work3-container");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 10;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
